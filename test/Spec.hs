58f5cd13966a9c5501d7a81585791b16a7fc64c8
Elben Shira <elbenshira@gmail.com>
2016-05-30 15:00:26 -0500

    Simplify Arbitrary Term.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,6 +1,4 @@
 import Test.QuickCheck
-import Test.QuickCheck.Arbitrary
-import System.Random
 
 import Lib
 
@@ -17,26 +15,22 @@ prop_revrev xs = reverse xs == xs
 instance Arbitrary Term where
   -- arbitrary:: Gen Term
   arbitrary = do
-    n <- choose (1, 3) :: Gen Int
+    n <- choose (1, 2) :: Gen Int
     case n of
       1 -> do depth <- choose (2, 4) :: Gen Int
               t <- arbitraryWithDepth depth
               return $ Not t
       2 -> do depth <- choose (2, 4) :: Gen Int
-              m <- choose (2,3) :: Gen Int -- 2 to 3 terms
-              terms <- mapM (const (arbitraryWithDepth depth)) [1..m]
-              return $ And terms
-      3 -> do depth <- choose (2, 4) :: Gen Int
-              m <- choose (2,3) :: Gen Int -- 2 to 3 terms
-              terms <- mapM (const (arbitraryWithDepth depth)) [1..m]
-              return $ Or terms
+              arbitraryWithDepth depth
+
+  shrink = undefined
 
 arbitraryWithDepth :: Int -> Gen Term
 arbitraryWithDepth n =
   case n of
     -- At bottom. Only do vars.
-    0 -> do n <- choose ('a', 'z') :: Gen Char
-            return $ Var [n]
+    0 -> do v <- choose ('a', 'z') :: Gen Char
+            return $ Var [v]
     _ -> do
       m <- choose (1, 4) :: Gen Int
       case m of
@@ -44,11 +38,11 @@ arbitraryWithDepth n =
                 return $ Var [v]
         2 -> do t <- arbitraryWithDepth (n-1)
                 return $ Not t
-        3 -> do m <- choose (1,1) :: Gen Int -- 2 to 3 terms
-                terms <- mapM (const (arbitraryWithDepth (n-1))) [0..m]
+        3 -> do numTerms <- choose (1,1) :: Gen Int -- 2 to 3 terms
+                terms <- mapM (const (arbitraryWithDepth (n-1))) [0..numTerms]
                 return $ And terms
-        4 -> do m <- choose (1,1) :: Gen Int -- 2 to 3 terms
-                terms <- mapM (const (arbitraryWithDepth (n-1))) [0..m]
+        4 -> do numTerms <- choose (1,1) :: Gen Int -- 2 to 3 terms
+                terms <- mapM (const (arbitraryWithDepth (n-1))) [0..numTerms]
                 return $ Or terms
 
 -- :m +Test.QuickCheck.Arbitrary
@@ -56,3 +50,4 @@ arbitraryWithDepth n =
 -- sample' arbitrary :: IO [Bool]
 -- sample' arbitrary :: IO [Term]
 -- generate $ vectorOf 1 arbitrary :: IO [Term]
+-- generate arbitrary :: IO [Term]
